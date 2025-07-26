ea3b01b3ea825ed74a474a8a09f88c72876829aa
Chirplett <chirplett@gmail.com>
2025-07-27 04:09:08 +0900

    chore: add ViewDesignProtocol for consistent view configuration - Defined configureHierarchy, configureView, and configureLayout methods - Created ViewDesignProtocol.swift for structuring view setup logic

---
+++ SearchToBuy/ViewDesignProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  ViewDesignProtocol.swift
+//  SearchToBuy
+//
+//  Created by Jude Song on 7/27/25.
+//
+
+import Foundation
+
+protocol ViewDesignProtocol {
+    
+    func configureHierarchy()
+    func configureView()
+    func configureLayout()
+    
+    
+}
