143cba08a8685f786edd7ca89267a73add175898
Chirplett <chirplett@gmail.com>
2025-07-27 02:43:46 +0900

    chore: initialize SearchToBuy project with core view controllers and SnapKit
    - Created MainViewController and SearchResultViewController
    - Added SnapKit package for layout management

---
+++ SearchToBuy/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  SearchToBuy
+//
+//  Created by Jude Song on 7/27/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
---
+++ SearchToBuy/SearchResultViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchResultViewController.swift
+//  SearchToBuy
+//
+//  Created by Jude Song on 7/27/25.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
