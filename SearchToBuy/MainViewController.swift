5d9eca272b43ca18b095955af7df9c2f01506465
Chirplett <chirplett@gmail.com>
2025-07-27 04:12:59 +0900

    feat: add search bar to MainViewController with SnapKit and design protocol - Implemented UISearchBar with placeholder and default style - Used ViewDesignProtocol to structure hierarchy, view, and layout - Applied SnapKit for precise search bar layout within safe area - Embedded MainViewController in UINavigationController via storyboard - Set navigation title to "영캠러의 쇼핑쇼핑"

--- SearchToBuy/MainViewController.swift
+++ SearchToBuy/MainViewController.swift
@@ -6,15 +6,49 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
+    
+    private let searchBar: UISearchBar = {
+       let searchBar = UISearchBar()
+        searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: "브랜드, 상품, 프로필, 태그 등", attributes: [.foregroundColor: UIColor.lightGray])
+        searchBar.searchBarStyle = .default
+        return searchBar
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureHierarchy()
+        configureView()
+        configureLayout()
 
        
     }
     
 
 
+}
+
+extension MainViewController: ViewDesignProtocol {
+    
+    func configureHierarchy() {
+        view.addSubview(searchBar)
+    }
+    
+    func configureView() {
+        navigationItem.title = "영캠러의 쇼핑쇼핑"
+    }
+    
+    func configureLayout() {
+        searchBar.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(8)
+            make.height.equalTo(44)
+        }
+    }
+    
+    
+    
+    
 }
